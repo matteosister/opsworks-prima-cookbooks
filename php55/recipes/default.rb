b4689ac46c24a9899d2c2c27a777ff52b9e90e94
Matteo <matteog@gmail.com>
2014-02-24 22:32:58 +0100

    command

--- php55/recipes/default.rb
+++ php55/recipes/default.rb
@@ -3,10 +3,6 @@
 # Recipe:: default
 #
 
-#include_recipe "apt"
+include_recipe "apt"
 
-apt_repository "ondrej-php5" do
-  uri "http://ppa.launchpad.net/ondrej/php5/ubuntu"
-  distribution "precise"
-  action :add
-end
+command "add-apt-repository -y ppa:ondrej/php5"
