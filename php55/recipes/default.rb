61d0fb44aa6023a2dd74dfc10067ec04021238c2
Matteo <matteog@gmail.com>
2014-02-24 22:43:23 +0100

    no each

--- php55/recipes/default.rb
+++ php55/recipes/default.rb
@@ -1,6 +1,4 @@
-node[:deploy].each do |app_name, deploy|
-  execute "add apt repository" do
-    command "add-apt-repository -y ppa:ondrej/php5"
-    action :run
-  end
+execute "add apt repository" do
+  command "add-apt-repository -y ppa:ondrej/php5"
+  action :run
 end
